dba9a2941599972305f34cac27888740705a1c2b
Jemster07 <emelie.kerek@gmail.com>
2024-04-26 14:19:11 -0400

    got datatables rows to load data

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const apiURL = 'https://api.artic.edu/api/v1/artworks';
-
-$(document).ready( function () {
-    $('#artTable').DataTable( {
+$(document).ready(function () {
+    
+    var table = $('#artTable').DataTable({
         ajax: {
-            url: apiURL,
-            dataSrc: 'data'
+            url: 'https://api.artic.edu/api/v1/artworks',
+            dataSrc: 'data',
         },
         columns: [
+            { data: 'id' },
             { data: 'artist_title' },
             { data: 'title' },
             { data: 'classification_title' },
             { data: 'date_display' }
-        ]
+        ],
+        columnDefs: [
+            {
+                targets: 0,
+                searchable: false,
+                visible: false,
+            }
+        ],
     });
-} );
+
+    $('tbody').on('click', 'tr', function () {
+        console.log(table.row(this).data());
+    });
+
+});
